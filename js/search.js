215cd9a56a1326b5d5fea74fa6b8bcf2db382d02
rusakroman <rrusak87@gmail.com>
2022-02-24 00:23:03 +0300

    Усложненное задание

--- js/search.js
+++ js/search.js
@@ -1,9 +1,24 @@
-const search = function() {
+// Повторение по уроку
+// const search = function() {
+//   const input = document.querySelector('.search-block > input')
+//   const searchBtn = document.querySelector('.search-block > button')
+
+//   input.addEventListener('input', (event) => {
+//     console.log(event.target.value);
+
+//   })
+// }
+
+// search()
+
+
+// Усложненное задание
+  const search = function() {
   const input = document.querySelector('.search-block > input')
   const searchBtn = document.querySelector('.search-block > button')
 
-  input.addEventListener('input', (event) => {
-    console.log(event.target.value);
+  searchBtn.addEventListener('click', () => {
+    console.log(input.value);
 
   })
 }
